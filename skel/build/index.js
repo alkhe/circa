eff989448875239e17995852a650afc100c63836
edge <sort@programmer.net>
2015-09-13 12:22:20 +0900

    remove gulp dependency

--- skel/build/index.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-import jade from 'gulp-jade';
-
-gulp.task('html', () =>
-	gulp.src('./src/html/*.jade')
-		.pipe(jade({ compileDebug: false }))
-		.pipe(gulp.dest('./public'))
-);
