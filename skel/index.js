9db5aca74341e0ed9cff37d363bc63d8cd29e1d0
edge <sort@programmer.net>
2015-09-13 09:10:00 +0900

    implement isomorphic rendering

--- skel/index.js
+++ skel/index.js
@@ -2,34 +2,38 @@ import express from 'express';
 import webpack from 'webpack';
 
 let production = process.env.NODE_ENV === 'production';
+
 let app = express();
+let router = express.Router();
 
 if (production) {
 	console.log('[pro]');
 	app.use(require('compression')());
 
-	// let React = require('react'),
-	// 	RDOM = require('react-dom'),
-	// 	{ RoutingContext, match } = require('react-router'),
-	// 	createLocation = require('history/lib/createLocation'),
-	// 	Flux = require('fluxette'),
-	// 	reducer = require('./src/js/flux/reducer'),
-	// 	Routes = require('./src/js/routes'),
-	// 	{ Context } = require('fluxette-react');
-	//
-	// app.use('/', (req, res) => {
-	// 	let flux = Flux(reducer);
-	// 	let location = createLocation(req.url);
-	// 	match({ routes: Routes, location }, (err, redir, initial) => {
-	// 		res.send(
-	// 			RDOM.renderToString(
-	// 				<Context flux={ flux }>
-	// 					<RoutingContext { ...initial } />
-	// 				</Context>
-	// 			)
-	// 		);
-	// 	})
-	// });
+	let jade = require('jade'),
+		React = require('react'),
+		RDOM = require('react-dom/server'),
+		{ RoutingContext, match } = require('react-router'),
+		createLocation = require('history/lib/createLocation'),
+		Flux = require('fluxette'),
+		reducer = require('./src/js/flux/reducer'),
+		Routes = require('./src/js/routes'),
+		{ Context } = require('fluxette-react');
+
+	let template = jade.compileFile('./src/html/index.jade');
+	let iso = root => template({ ssr: RDOM.renderToString(root) });
+
+	router.get('/', (req, res) => {
+		let flux = Flux(reducer);
+		let location = createLocation(req.url);
+		match({ routes: Routes(), location }, (err, redir, initial) => {
+			res.send(iso(
+				<Context flux={ flux }>
+					{ () => <RoutingContext { ...initial } /> }
+				</Context>
+			));
+		})
+	});
 }
 else {
 	console.log('[dev]');
@@ -44,7 +48,9 @@ else {
 	})).use(hot(compiler));
 }
 
-app.use(express.static('./public'));
+app
+	.use(router)
+	.use(express.static('./public'));
 
 const port = process.env.PORT || 80;
 
